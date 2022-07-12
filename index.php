775178818f6509536ca155d168febff4cd429564
farhandwi <farhandwi234@gmail.com>
2022-07-12 20:00:56 +0700

    menambahkan paragraf lorem

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure vel, laboriosam corporis laborum quas voluptatum delectus nihil accusamus quod eos magnam enim sit est totam? Sequi in ut deserunt.</p>
 </body>
 </html>
