11b1fa98aada84d8d21e9822ceb6271b61937a64
farhandwi <farhandwi234@gmail.com>
2022-07-12 20:06:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure vel, laboriosam corporis laborum quas voluptatum delectus nihil accusamus quod eos magnam enim sit est totam? Sequi in ut deserunt.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
